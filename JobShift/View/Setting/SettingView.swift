2b76b7ae3c97f11d43481856707d835d1c53063c
shinking02 <k.shin0102@gmail.com>
2023-12-11 18:50:21 +0900

    スプラッシュ画面でのログイン, 復元, スコープ要求実装

---
+++ JobShift/View/Setting/SettingView.swift
@@ -0,0 +1,8 @@
+//
+//  SettingView.swift
+//  JobShift
+//
+//  Created by 川上真 on 2023/12/10.
+//
+
+import Foundation
---
+++ JobShift/ViewModel/CalendarStore.swift
@@ -0,0 +1,8 @@
+//
+//  CalendarStore.swift
+//  JobShift
+//
+//  Created by 川上真 on 2023/12/10.
+//
+
+import Foundation
