4bfd92b7b2de93faab3f40ab575df56e2babbb9f
shinking02 <k.shin0102@gmail.com>
2024-03-23 16:55:25 +0900

    Update post Xcode build script to handle new commit message

--- ci_scripts/ci_post_xcodebuild.sh
+++ ci_scripts/ci_post_xcodebuild.sh
@@ -1,12 +1,12 @@
 #!/bin/zsh
 #  ci_post_xcodebuild.sh
 
-# COMMIT=$(git fetch --deepen 3 && git log -3 --pretty=format:"%s")
+COMMIT=$(git fetch --deepen 3 && git log -3 --pretty=format:"%s")
 
-# WHAT_TO_TEST="[COMMIT]\n$COMMIT"
+WHAT_TO_TEST="[COMMIT]\n$COMMIT"
 
-# if [[ -d "$CI_APP_STORE_SIGNED_APP_PATH" ]]; then
-#   TESTFLIGHT_DIR_PATH=../TestFlight
-#   mkdir $TESTFLIGHT_DIR_PATH
-#   echo "$WHAT_TO_TEST" >> $TESTFLIGHT_DIR_PATH/WhatToTest.en-US.txt
-# fi
+if [[ -d "$CI_APP_STORE_SIGNED_APP_PATH" ]]; then
+  TESTFLIGHT_DIR_PATH=../TestFlight
+  mkdir $TESTFLIGHT_DIR_PATH
+  echo "$WHAT_TO_TEST" >> $TESTFLIGHT_DIR_PATH/WhatToTest.en-US.txt
+fi
