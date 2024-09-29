c521014b8fe33f492712e231d4e16283e8d9f595
shinking02 <k.shin0102@gmail.com>
2024-09-29 12:52:17 +0900

    Add APIKeyManager for handling API keys

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -1,12 +1,3 @@
-#!/bin/bash
+#!/bin/sh
 
 defaults write com.apple.dt.Xcode IDESkipPackagePluginFingerprintValidatation -bool YES
-
-env_file_path="../JobShift/Env.swift"
-
-typeset -A envValues
-
-envValues[GITHUB_API_TOKEN]=$GITHUB_API_TOKEN
-
-for key in ${(k)envValues}
-  sed -i -e "s/${key}/${envValues[$key]}/g" "${env_file_path}"
